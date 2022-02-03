4f84eba96f458eb4c11210a8299cd6aef028a4de
макс макс <giniyatulinm@mail.ru>
2022-02-03 18:46:46 +0500

    Created DB and Connected it to app Personal Tracking

--- MSSQL with WPF Core/ReadMe.md
+++ MSSQL with WPF Core/ReadMe.md
@@ -1,3 +1,8 @@
 MSSQL with C# WPF Core Project, MVVM by Using EF Core
 
 https://www.udemy.com/course/mssql-with-c-wpf-core-project-mvvm-by-using-ef-core/
+
+
+Создание моделей данных из БД
+1. Подключение через обозреватель данных
+2. Прописать в консоли диспетчера пакетов - scaffold-DbContext "Server=.; Database=PERSONALTRACKING; trusted_connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir DB
---
+++ MSSQL with WPF Core/WPFPERSONALTRACKING/WPFPERSONALTRACKING/DB/Month.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WPFPERSONALTRACKING.DB
+{
+    public partial class Month
+    {
+        public Month()
+        {
+            Salaries = new HashSet<Salary>();
+        }
+
+        public int Id { get; set; }
+        public string MonthName { get; set; }
+
+        public virtual ICollection<Salary> Salaries { get; set; }
+    }
+}
